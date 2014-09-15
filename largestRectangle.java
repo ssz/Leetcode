688b5eec055d2a7643b05f78ea96a5aa0da27a0f
ssz <zish.zhang@gmail.com>
2014-09-15 15:01:57 -0700

    Sep 15

--- largestRectangle.java
+++ largestRectangle.java
@@ -4,31 +4,33 @@
 	For example, Given height = [2,1,5,6,2,3], return 10.	
 
 	Solution: O(n)
+    Reference: http://www.cnblogs.com/lichen782/p/leetcode_Largest_Rectangle_in_Histogram.html
+ 
 */
 
 public class Solution {
     public int largestRectangleArea(int[] height) {
-        
         if(height == null || height.length==0){
             return 0;
         }
-    
-        Stack<Integer> st = new Stack<Integer>();
-        Stack<Integer> restack = new Stack<Integer>(); 
-        st.push(height[0]);
-        int l=1;
-        int N = height.length;
-        for(int i=1; i<N; i++){
-            int oldH=st.pop();
-            if(oldH >= height[i]){
-                st.push(oldH*l);
-                l++;
-            }else{
-                l=1;
-                st.push(height[i]);
-            }
-        }
-        return l*st.pop();
         
+        int[] h = new int[height.length + 1];
+        h = Arrays.copyOf(height, height.length + 1);
+        //height[height.length+1]=0;
+        
+        Stack<Integer> st = new Stack<Integer>();
+        int r=0;
+        int max=0;
+        while(r<h.length){
+        	if(st.isEmpty() || h[st.peek()] <= h[r]){
+        		st.push(r++);
+        	}else{
+        		int t = st.pop();
+        		int len = st.isEmpty() ? r : r - st.peek() - 1;
+        		max = Math.max(max, h[t] * len);
+        	}
+        }
+        
+        return max;
     }
 }
