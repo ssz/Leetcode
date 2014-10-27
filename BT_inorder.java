ae42009f9197d530e2fbd5c3272f5126c76f62dc
ssz <zish.zhang@gmail.com>
2014-10-26 23:01:41 -0700

    tree

---
+++ BT_inorder.java
@@ -0,0 +1,19 @@
+/* 	Binary Tree Inorder Traversal */
+/*	Given a binary tree, return the inorder traversal of its nodes' values.
+
+	For example: Given binary tree {1,#,2,3},
+		1
+	    \
+	     2
+	    /
+	   3
+	return [1,3,2].
+	Note: Recursive solution is trivial, could you do it iteratively?
+*/
+
+// Questions about traversal is often solved by recursion and stack+iterative.
+// Another solution is Morris Traversal.
+// 1. Iterative way (stack).   Time: O(n), Space: O(n).
+// 2. Recursive solution.      Time: O(n), Space: O(n).
+// 3. Threaded tree (Morris).  Time: O(n), Space: O(1).
+
