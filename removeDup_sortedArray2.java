2c106aa54bd99035d7ae441ceb4543876b97a851
ssz <zish.zhang@gmail.com>
2014-10-28 15:45:47 -0700

    Array

--- removeDup_sortedArray2.java
+++ removeDup_sortedArray2.java
@@ -9,4 +9,42 @@
 	Your function should return length = 5, and A is now [1,1,2,2,3].
 */
 
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        if(A==null || A.length==0) return 0;
+        if(A.length<=2) return A.length;
+        int i=2, index=2;
+        for(;i<A.length;i++){
+            if(A[i]==A[index-1] && A[i]==A[index-2]) continue;
+            else A[index++]=A[i];
+        }
+        return index;
+    }
+}
+
+// Maintain two pointers to track the index in original array and result array
+// And we need a counter to track the occurrence of the value
+// time: O(n); space: (1)
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        if (A==null || A.length<=0)
+            return 0;
+        int i = 1, j = 1, N = A.length, count = 1;
+        while (j < N){
+           if (A[j]!=A[j-1]){
+                A[i++] = A[j++];
+                count =1;
+           }else{
+                if (count < 2)
+                    A[i++] = A[j++];
+                else  // count>=2, only move forward j
+                    j++;
+                count++;
+           }
+        }
+        return i;
+    }
+}
+
+
 
