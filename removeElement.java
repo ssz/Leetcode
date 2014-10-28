99a20d62959d69f4421f0f1057d6ac16364305ce
ssz <zish.zhang@gmail.com>
2014-10-27 21:49:38 -0700

    Array

--- removeElement.java
+++ removeElement.java
@@ -5,3 +5,37 @@
 	The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
 */
+
+// overwrite the target elem
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        if(A==null || A.length==0) return 0;
+        int l=A.length;
+        int index=0;
+        for(int i=0; i<l; i++){
+            if(A[i] !=elem){
+                A[index++]=A[i];
+            }
+        }
+        return index;
+    }
+}
+
+// swap target elem to the end of the array
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        if (A==null || A.length==0) return 0;
+        int i=0, j=A.length-1;
+        while (i <= j){
+            if (A[i] == elem)   swap(A, i, j--);
+            else i++;
+        }
+        return j+1;
+    }
+    
+    public void swap(int[] A, int i, int j){
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+    }
+}
