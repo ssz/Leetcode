b6fc4d1f90d63af370bab4b1398b8eae75e46189
ssz <zish.zhang@gmail.com>
2014-11-02 19:56:02 -0800

    linklist

---
+++ insertion_sortlist.java
@@ -0,0 +1,47 @@
+/*
+    Insertion Sort List
+    Sort a linked list using insertion sort.
+*/ 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode newhead = new ListNode(Integer.MIN_VALUE);
+        newhead.next = head;
+        ListNode begin = head.next;
+        head.next = null; // cut the list, in case of infinite loop.
+        while(begin != null){
+            ListNode tem = newhead;
+            while(tem.next != null){
+                if(tem.next.val < begin.val){
+                    tem = tem.next;
+                }else{
+                    break;
+                }
+            }
+            
+            if(tem.next == begin){
+                // tem.val < begin.val, do not need to change position.
+                tem.next = begin;
+                begin = begin. next;
+            }else{
+                // change position (tem.next and begin)
+                ListNode node = begin.next;
+                begin.next = tem.next;
+                tem.next = begin;
+                begin = node;
+            }
+        }
+        return newhead.next;
+    }
+}
---
+++ removeDup_sortedlist2.java
@@ -0,0 +1,73 @@
+/*
+	Remove Duplicates from Sorted List II 
+
+	Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+	For example,
+	Given 1->2->3->3->4->4->5, return 1->2->5.
+	Given 1->1->1->2->3, return 2->3.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode curr = head;
+        ListNode p = new ListNode(0);
+        ListNode result = new ListNode(0);
+        ListNode new_head = result;
+        //result.next = head;
+        
+        while(curr != null){
+            //ListNode trackt = curr.next;
+            p = curr.next;
+            while (p != null && p.val == curr.val) p = p.next;
+            if (curr.next == p) {  // if equal (no movement), no duplicates here.
+                ListNode node = new ListNode(curr.val);
+                result.next = node;
+                result = node;
+                //add node
+            }
+            curr = p;
+        }
+        return new_head.next;
+    }
+}
+
+// The following solution comes from Tao Hu.
+// keep a prev pointer, if the following nodes are duplicates, remove them; 
+// otherwise, move the prev pointer forwrd
+// time: O(n); space: O(1)
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head==null || head.next==null)
+            return head;
+        ListNode sen = new ListNode(0); sen.next = head;
+        ListNode pp = sen, p = head;
+        while (p!=null && p.next!=null){
+            ListNode q = p.next;
+            while (q!=null && q.val==p.val)    q = q.next;
+            if (q!=p.next)
+                pp.next = q;
+            else
+                pp = p;
+            p = q;
+        }
+        return sen.next;
+    }
+}
+
+
+
+
